952af097b4933de67c07057b57f72eb2d05aa5a9
k45lim <k45lim@gmail.com>
2024-02-08 13:11:26 -0700

    Add files via upload

---
+++ EDAHelper.py
@@ -0,0 +1,366 @@
+import time
+import datetime
+import re
+import string
+import sys
+import pandas as pd
+import textblob
+from textblob import TextBlob
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import wordnet
+
+# Download necessary NLTK resources
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+STOPWORDS = stopwords.words("english")
+
+###################################################
+#  CONFIGURATION SECTION
+# OPTION_LEMMATIZE = True to output Lemmatized words
+# OPTION_LEMMATIZE = False to output Stemming words
+###################################################
+OPTION_LEMMATIZE = False
+
+PUNCTUATION_MARKS = string.punctuation
+
+CHAT_WORDS = {
+    "$" : " dollar ",
+    "€" : " euro ",
+    "4ao" : "for adults only",
+    "a.m" : "before midday",
+    "a3" : "anytime anywhere anyplace",
+    "aamof" : "as a matter of fact",
+    "acct" : "account",
+    "adih" : "another day in hell",
+    "afaic" : "as far as i am concerned",
+    "afaict" : "as far as i can tell",
+    "afaik" : "as far as i know",
+    "afair" : "as far as i remember",
+    "afk" : "away from keyboard",
+    "app" : "application",
+    "approx" : "approximately",
+    "apps" : "applications",
+    "asap" : "as soon as possible",
+    "asl" : "age, sex, location",
+    "atk" : "at the keyboard",
+    "ave." : "avenue",
+    "aymm" : "are you my mother",
+    "ayor" : "at your own risk", 
+    "b&b" : "bed and breakfast",
+    "b+b" : "bed and breakfast",
+    "b.c" : "before christ",
+    "b2b" : "business to business",
+    "b2c" : "business to customer",
+    "b4" : "before",
+    "b4n" : "bye for now",
+    "b@u" : "back at you",
+    "bae" : "before anyone else",
+    "bak" : "back at keyboard",
+    "bbbg" : "bye bye be good",
+    "bbc" : "british broadcasting corporation",
+    "bbias" : "be back in a second",
+    "bbl" : "be back later",
+    "bbs" : "be back soon",
+    "be4" : "before",
+    "bfn" : "bye for now",
+    "blvd" : "boulevard",
+    "bout" : "about",
+    "brb" : "be right back",
+    "bros" : "brothers",
+    "brt" : "be right there",
+    "bsaaw" : "big smile and a wink",
+    "btw" : "by the way",
+    "bwl" : "bursting with laughter",
+    "c/o" : "care of",
+    "cet" : "central european time",
+    "cf" : "compare",
+    "cia" : "central intelligence agency",
+    "csl" : "can not stop laughing",
+    "cu" : "see you",
+    "cul8r" : "see you later",
+    "cv" : "curriculum vitae",
+    "cwot" : "complete waste of time",
+    "cya" : "see you",
+    "cyt" : "see you tomorrow",
+    "dae" : "does anyone else",
+    "dbmib" : "do not bother me i am busy",
+    "diy" : "do it yourself",
+    "dm" : "direct message",
+    "dwh" : "during work hours",
+    "e123" : "easy as one two three",
+    "eet" : "eastern european time",
+    "eg" : "example",
+    "embm" : "early morning business meeting",
+    "encl" : "enclosed",
+    "encl." : "enclosed",
+    "etc" : "and so on",
+    "faq" : "frequently asked questions",
+    "fawc" : "for anyone who cares",
+    "fb" : "facebook",
+    "fc" : "fingers crossed",
+    "fig" : "figure",
+    "fimh" : "forever in my heart", 
+    "ft." : "feet",
+    "ft" : "featuring",
+    "ftl" : "for the loss",
+    "ftw" : "for the win",
+    "fwiw" : "for what it is worth",
+    "fyi" : "for your information",
+    "g9" : "genius",
+    "gahoy" : "get a hold of yourself",
+    "gal" : "get a life",
+    "gcse" : "general certificate of secondary education",
+    "gfn" : "gone for now",
+    "gg" : "good game",
+    "gl" : "good luck",
+    "glhf" : "good luck have fun",
+    "gmt" : "greenwich mean time",
+    "gmta" : "great minds think alike",
+    "gn" : "good night",
+    "g.o.a.t" : "greatest of all time",
+    "goat" : "greatest of all time",
+    "goi" : "get over it",
+    "gps" : "global positioning system",
+    "gr8" : "great",
+    "gratz" : "congratulations",
+    "gyal" : "girl",
+    "h&c" : "hot and cold",
+    "hp" : "horsepower",
+    "hr" : "hour",
+    "hrh" : "his royal highness",
+    "ht" : "height",
+    "ibrb" : "i will be right back",
+    "ic" : "i see",
+    "icq" : "i seek you",
+    "icymi" : "in case you missed it",
+    "idc" : "i do not care",
+    "idgadf" : "i do not give a damn fuck",
+    "idgaf" : "i do not give a fuck",
+    "idk" : "i do not know",
+    "ie" : "that is",
+    "i.e" : "that is",
+    "ifyp" : "i feel your pain",
+    "IG" : "instagram",
+    "iirc" : "if i remember correctly",
+    "ilu" : "i love you",
+    "ily" : "i love you",
+    "imho" : "in my humble opinion",
+    "imo" : "in my opinion",
+    "imu" : "i miss you",
+    "iow" : "in other words",
+    "irl" : "in real life",
+    "j4f" : "just for fun",
+    "jic" : "just in case",
+    "jk" : "just kidding",
+    "jsyk" : "just so you know",
+    "l8r" : "later",
+    "lb" : "pound",
+    "lbs" : "pounds",
+    "ldr" : "long distance relationship",
+    "lmao" : "laugh my ass off",
+    "lmfao" : "laugh my fucking ass off",
+    "lol" : "laughing out loud",
+    "ltd" : "limited",
+    "ltns" : "long time no see",
+    "m8" : "mate",
+    "mf" : "motherfucker",
+    "mfs" : "motherfuckers",
+    "mfw" : "my face when",
+    "mofo" : "motherfucker",
+    "mph" : "miles per hour",
+    "mr" : "mister",
+    "mrw" : "my reaction when",
+    "ms" : "miss",
+    "mte" : "my thoughts exactly",
+    "nagi" : "not a good idea",
+    "nbc" : "national broadcasting company",
+    "nbd" : "not big deal",
+    "nfs" : "not for sale",
+    "ngl" : "not going to lie",
+    "nhs" : "national health service",
+    "nrn" : "no reply necessary",
+    "nsfl" : "not safe for life",
+    "nsfw" : "not safe for work",
+    "nth" : "nice to have",
+    "nvr" : "never",
+    "nyc" : "new york city",
+    "oc" : "original content",
+    "og" : "original",
+    "ohp" : "overhead projector",
+    "oic" : "oh i see",
+    "omdb" : "over my dead body",
+    "omg" : "oh my god",
+    "omw" : "on my way",
+    "p.a" : "per annum",
+    "p.m" : "after midday",
+    "pm" : "prime minister",
+    "poc" : "people of color",
+    "pov" : "point of view",
+    "pp" : "pages",
+    "ppl" : "people",
+    "prw" : "parents are watching",
+    "ps" : "postscript",
+    "pt" : "point",
+    "ptb" : "please text back",
+    "pto" : "please turn over",
+    "qpsa" : "what happens", #"que pasa",
+    "ratchet" : "rude",
+    "rbtl" : "read between the lines",
+    "rlrt" : "real life retweet", 
+    "rofl" : "rolling on the floor laughing",
+    "roflol" : "rolling on the floor laughing out loud",
+    "rotflmao" : "rolling on the floor laughing my ass off",
+    "rt" : "retweet",
+    "ruok" : "are you ok",
+    "sfw" : "safe for work",
+    "sk8" : "skate",
+    "smh" : "shake my head",
+    "sq" : "square",
+    "srsly" : "seriously", 
+    "ssdd" : "same stuff different day",
+    "tbh" : "to be honest",
+    "tbs" : "tablespooful",
+    "tbsp" : "tablespooful",
+    "tfw" : "that feeling when",
+    "thks" : "thank you",
+    "tho" : "though",
+    "thx" : "thank you",
+    "tia" : "thanks in advance",
+    "til" : "today i learned",
+    "tl;dr" : "too long i did not read",
+    "tldr" : "too long i did not read",
+    "tmb" : "tweet me back",
+    "tntl" : "trying not to laugh",
+    "ttyl" : "talk to you later",
+    "u" : "you",
+    "u2" : "you too",
+    "u4e" : "yours for ever",
+    "utc" : "coordinated universal time",
+    "w/" : "with",
+    "w/o" : "without",
+    "w8" : "wait",
+    "wassup" : "what is up",
+    "wb" : "welcome back",
+    "wtf" : "what the fuck",
+    "wtg" : "way to go",
+    "wtpa" : "where the party at",
+    "wuf" : "where are you from",
+    "wuzup" : "what is up",
+    "wywh" : "wish you were here",
+    "yd" : "yard",
+    "ygtr" : "you got that right",
+    "ynk" : "you never know",
+    "zzz" : "sleeping bored and tired"
+}
+
+
+# SHOW TIMESTAMP
+def show_time():
+    timestamp = time.time()
+    dt_object = datetime.datetime.fromtimestamp(timestamp)
+    human_readable_time = dt_object.strftime('%Y-%m-%d %H:%M:%S')
+    return human_readable_time
+
+# LOWER ALL CASES
+def lower_cases(text):
+    return text.lower()
+
+# REMOVE HTML TAGS IN THE TEXT
+def remove_html_tags(text):
+    pattern=re.compile('<.*?>')
+    return pattern.sub("",text)
+
+# REMOVE URL IN THE TEXT	
+def remove_url(text):
+    pattern=re.compile(r'https?://\S+|www\.\S+')
+    return pattern.sub("",text)
+	
+
+# REMOVE PUNCTUATION MARKS IN THE TEXT
+def remove_punc(text):
+    for char in PUNCTUATION_MARKS:
+        text=text.replace(char,"")
+    return text
+
+# EXPOUND FREQUENTLY USED CHAT ABBREVIATION
+# CHAT WORDS TAKEN FROM https://www.kaggle.com/code/nmaguette/up-to-date-list-of-slangs-for-text-preprocessing/notebook
+# INTERESTINGLY, it can remove all the URL links?! - need to test using the provided site
+def chat_conversion(text):
+    new_text=[]
+    for w in text.split():
+        if w in CHAT_WORDS:
+            new_text.append(CHAT_WORDS[w])
+        else:
+            new_text.append(w)
+    return " ".join(new_text)
+	
+def convert_abbrev(word):
+    return abbreviations[word.lower()] if word.lower() in abbreviations.keys() else word
+	
+
+# SPELL CORRECTION  - need to convert this into function
+
+def spell_correction(text):
+    txtblob=TextBlob(text)
+    return (txtblob.correct().string)
+
+# REMOVE STOPWORDS
+
+def remove_stopwords(text):
+    # Tokenize the text into words
+    words = word_tokenize(text)
+    
+    # Get the English stopwords
+    # english_stopwords = set(stopwords.words('english'))
+    
+    # Filter out the stopwords from the text
+    filtered_words = [word for word in words if word.lower() not in STOPWORDS]
+    
+    # Join the filtered words back into a single string
+    filtered_text = ' '.join(filtered_words)
+    return filtered_text
+	
+# TRANSCRIBE EMOJIS
+def remove_emoji(text):
+    clean_text=emoji.demojize(text)
+    return clean_text
+	
+import re
+def remove_emojis_manually(text):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F700-\U0001F77F"  # alchemical symbols
+                               u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+                               u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+                               u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+                               u"\U0001FA00-\U0001FA6F"  # Chess Symbols
+                               u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+                               u"\U00002702-\U000027B0"  # Dingbats
+                               u"\U000024C2-\U0001F251" 
+                               "]+", flags=re.UNICODE)
+    clean_text = emoji_pattern.sub(r'', text)
+    return clean_text
+
+
+def preprocess_text(text):
+    # Tokenization
+    tokens = word_tokenize(text)
+    
+    # Stemming (optional)
+    stemmer = PorterStemmer()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+    result = lemmatized_tokens if OPTION_LEMMATIZE else stemmed_tokens
+    
+    # return tokens, stemmed_tokens, lemmatized_tokens
+    return result
